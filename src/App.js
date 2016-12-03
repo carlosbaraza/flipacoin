fd883d2356fdb6f1e4adbba88d04a945c0cfe17e
Carlos Baraza Haro <carlos.baraza@ieee.org>
2016-12-03 22:06:00 +0000

    Remove css debug log

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.scss';
 
-console.log(styles);
-
 class App extends Component {
   render() {
     return (
