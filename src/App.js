a9ad8d353bc1574739b3ffe27e5656f8fcfa7200
Carlos Baraza Haro <carlos.baraza@ieee.org>
2016-12-04 18:20:31 +0000

    Add THREE and 100 pesetas coin model

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Coin from './Coin';
 import styles from './App.scss';
+import THREE from 'three';
+import React3 from 'react-three-renderer';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    // construct the position vector here, because if we use 'new' within render,
+    // React will think that things have changed when they have not.
+    this.cameraPosition = new THREE.Vector3(0, 0, 5);
+
+    this.state = {
+      coinRotation: new THREE.Euler(),
+    };
+
+    this.scenePosition = new THREE.Vector3(0, 0, 0);
+    this.directionalLightPosition = new THREE.Vector3(0, 5, 5);
+
+    this._onAnimate = () => {
+      // we will get this callback every frame
+
+      // pretend coinRotation is immutable.
+      // this helps with updates and pure rendering.
+      // React will be sure that the rotation has now updated.
+      this.setState({
+        coinRotation: new THREE.Euler(
+          this.state.coinRotation.x + 0.01,
+          this.state.coinRotation.y + 0.01,
+          0
+        ),
+      });
+    };
+  }
+
   render() {
+    const width = window.innerWidth; // canvas width
+    const height = window.innerHeight; // canvas height
+
     return (
       <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={styles.appIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <React3
+          mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
+          width={width}
+          height={height}
+          alpha
+          antialias
+          pixelRatio={window.devicePixelRatio}
+
+          onAnimate={this._onAnimate}
+        >
+          <scene>
+            <perspectiveCamera
+              name="camera"
+              fov={75}
+              aspect={width / height}
+              near={0.1}
+              far={1000}
+
+              position={this.cameraPosition}
+              lookAt={this.scenePosition}
+            />
+            <ambientLight
+              color={0x404040}
+            />
+            <directionalLight
+              color={0xffffff}
+              position={this.directionalLightPosition}
+              lookAt={this.scenePosition}
+            />
+            <Coin rotation={this.state.coinRotation} />
+          </scene>
+        </React3>
       </div>
     );
   }
---
+++ src/Coin.js
@@ -0,0 +1,192 @@
+import React, { Component } from 'react';
+import styles from './Coin.scss';
+import THREE from 'three';
+window.THREE = THREE;
+import React3 from 'react-three-renderer';
+
+class Coin extends Component {
+  static propTypes = {
+      rotation: React.PropTypes.object,
+  }
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.geometries = {};
+
+    this.radius = 1.0;
+    this.height = 0.2;
+    this.radialSegments = 40;
+    this.sideTextureRepeat = new THREE.Vector2(16, 1);
+
+    this.constructCaps();
+  }
+
+  constructCaps() {
+    this.caps = {
+      tails: {
+        vertices: [],
+        faceVertexUvs: [[]],
+        faces: [],
+      },
+      heads: {
+        vertices: [],
+        faceVertexUvs: [[]],
+        faces: [],
+      },
+    };
+
+    for (var i = 0; i < this.radialSegments; i++) {
+      var a = i * 1 / this.radialSegments * Math.PI * 2;
+      var z = Math.sin(a);
+      var x = Math.cos(a);
+      var a1 = (i + 1) * 1 / this.radialSegments * Math.PI * 2;
+      var z1 = Math.sin(a1);
+      var x1 = Math.cos(a1);
+
+      // Tails
+      this.caps.tails.vertices.push(
+        new THREE.Vector3(0, -this.height / 2, 0),
+        new THREE.Vector3(x * this.radius, -this.height / 2, z * this.radius),
+        new THREE.Vector3(x1 * this.radius, -this.height / 2, z1 * this.radius)
+      );
+      this.caps.tails.faceVertexUvs[0].push([
+        new THREE.Vector2(0.5, 0.5),
+        new THREE.Vector2(x / 2 + 0.5, z / 2 + 0.5),
+        new THREE.Vector2(x1 / 2 + 0.5, z1 / 2 + 0.5)
+      ]);
+      this.caps.tails.faces.push(new THREE.Face3(i * 3, i * 3 + 1, i * 3 + 2));
+
+      // Heads
+      this.caps.heads.vertices.push(
+        new THREE.Vector3(0, this.height / 2, 0),
+        new THREE.Vector3(x * this.radius, this.height / 2, z * this.radius),
+        new THREE.Vector3(x1 * this.radius, this.height / 2, z1 * this.radius)
+      );
+      this.caps.heads.faceVertexUvs[0].push([
+        new THREE.Vector2(0.5, 0.5),
+        new THREE.Vector2(x1 / 2 + 0.5, z1 / 2 + 0.5),
+        new THREE.Vector2(x / 2 + 0.5, z / 2 + 0.5)
+      ]);
+      this.caps.heads.faces.push(new THREE.Face3(i * 3, i * 3 + 2, i * 3 + 1));
+    }
+  }
+
+  componentDidMount() {
+    this.geometries.heads.computeFaceNormals();
+    this.geometries.tails.computeFaceNormals();
+  }
+
+  render() {
+    return (
+      <group>
+        <resources>
+          <meshPhongMaterial
+            resourceId="coinSideMaterial"
+            color={0xf9d38a}
+            side={THREE.DoubleSide}
+            bumpScale={0.2}
+          >
+            <texture
+              url="textures/coin-side-hd.jpg"
+              wrapS={THREE.RepeatWrapping}
+              wrapT={THREE.RepeatWrapping}
+              repeat={this.sideTextureRepeat}
+            />
+            <texture
+              url="textures/coin-side-bump-hd.jpg"
+              slot={'bumpMap'}
+              wrapS={THREE.RepeatWrapping}
+              wrapT={THREE.RepeatWrapping}
+              repeat={this.sideTextureRepeat}
+            />
+          </meshPhongMaterial>
+          <meshPhongMaterial
+            resourceId="coinTailsMaterial"
+            color={0xf9d38a}
+            side={THREE.DoubleSide}
+            bumpScale={0.1}
+          >
+            <texture
+              url="textures/coin-tails-hd.jpg"
+              wrapS={THREE.RepeatWrapping}
+              wrapT={THREE.RepeatWrapping}
+              repeat={this.textureRepeat}
+            />
+            <texture
+              url="textures/coin-tails-hd.jpg"
+              slot={'bumpMap'}
+              wrapS={THREE.RepeatWrapping}
+              wrapT={THREE.RepeatWrapping}
+              repeat={this.textureRepeat}
+            />
+          </meshPhongMaterial>
+          <meshPhongMaterial
+            resourceId="coinHeadsMaterial"
+            color={0xf9d38a}
+            side={THREE.DoubleSide}
+            bumpScale={0.1}
+          >
+            <texture
+              url="textures/coin-heads-hd.jpg"
+              wrapS={THREE.RepeatWrapping}
+              wrapT={THREE.RepeatWrapping}
+              repeat={this.textureRepeat}
+            />
+            <texture
+              url="textures/coin-heads-hd.jpg"
+              slot={'bumpMap'}
+              wrapS={THREE.RepeatWrapping}
+              wrapT={THREE.RepeatWrapping}
+              repeat={this.textureRepeat}
+            />
+          </meshPhongMaterial>
+        </resources>
+
+        <mesh
+          rotation={this.props.rotation}
+        >
+          <cylinderGeometry
+            ref={geo => this.geometries.side = geo}
+            radiusTop={this.radius}
+            radiusBottom={this.radius}
+            height={this.height}
+            radialSegments={this.radialSegments}
+            openEnded={1}
+          />
+          <materialResource
+            resourceId={'coinSideMaterial'}
+          />
+        </mesh>
+        <mesh
+          rotation={this.props.rotation}
+        >
+          <geometry
+            ref={geo => this.geometries.tails = geo}
+            faces={this.caps.tails.faces}
+            vertices={this.caps.tails.vertices}
+            faceVertexUvs={this.caps.tails.faceVertexUvs}
+          />
+          <materialResource
+            resourceId={'coinTailsMaterial'}
+          />
+        </mesh>
+        <mesh
+          rotation={this.props.rotation}
+        >
+          <geometry
+            ref={geo => this.geometries.heads = geo}
+            faces={this.caps.heads.faces}
+            vertices={this.caps.heads.vertices}
+            faceVertexUvs={this.caps.heads.faceVertexUvs}
+          />
+          <materialResource
+            resourceId={'coinHeadsMaterial'}
+          />
+        </mesh>
+      </group>
+    );
+  }
+}
+
+export default Coin;
